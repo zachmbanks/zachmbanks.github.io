7c65fa88f7ed9fc20faa7b528c711db540b48ddd
zachmbanks <zachmbanks@gmail.com>
2020-02-22 23:18:44 -0700

    first button impliment

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document.getElementById("button1").innerHTML = "Here We Go"
+
+
+function imageGenerate() {
+    document.getElementById("image").src="images/hand_sun.jpg"
+
+    
+}
+function name(params) {
+    
+}
